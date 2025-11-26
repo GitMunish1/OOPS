a4f99a45323d42663bbbfd095c3baeab617be03c
GitMunish1 <itsmrsduknow@gmail.com>
2025-11-26 16:23:47 +0530

    modification in the object properties
    we can manipulate the object properties

---
+++ method_1.py
@@ -0,0 +1,12 @@
+class happy:
+    def __init__ (self, net_worth, age):
+        self.net_worth = net_worth
+        self.age = age
+    def I_am_great(self):
+        self.net_worth += 10000000000000000000000000000000000000000
+        self.age += 10
+        print(f"My age is: {self.age}")
+        print(f"My net worth is: {self.net_worth}")
+Munish = happy(100000000000000000000, 25)
+Munish.I_am_great()
+Munish.I_am_great()
